a3717e64b1b924d1d0613e6e6f3cc645141aae1a
Evgenia Avramenko <hapjane@yandex.ru>
2024-10-23 16:36:00 +0500

    unit tests and mocks

---
+++ src/test/java/LionTest.java
@@ -0,0 +1,70 @@
+import com.example.Feline;
+import com.example.Lion;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.Spy;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import java.sql.SQLOutput;
+import java.util.List;
+import java.util.zip.CheckedOutputStream;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+@RunWith(MockitoJUnitRunner.class)
+public class LionTest {
+
+    @Spy
+    Feline feline;
+
+    @Test
+    public void getFoodTest() throws Exception {
+        Lion lion = new Lion(feline);
+        lion.getFood();
+        Mockito.verify(feline, Mockito.times(1)).getFood("Хищник");
+        List<String> food = List.of("Животные", "Птицы", "Рыба");
+        Mockito.when(feline.getFood("Хищник")).thenReturn(food);
+        assertEquals(food, lion.getFood());
+    }
+
+    @Test
+    public void getKittensTest() {
+        Lion lion = new Lion(feline);
+        lion.getKittens();
+        Mockito.verify(feline, Mockito.times(1)).getKittens();
+        Mockito.when(feline.getKittens()).thenReturn(1);
+        assertEquals(1, lion.getKittens());
+    }
+
+    @Test
+    public void doesHaveManeSamecReturnsTrue() throws Exception {
+        Lion lion = new Lion("Самец");
+        Assert.assertTrue(lion.doesHaveMane());
+    }
+
+    @Test
+    public void doesHaveManeSamecReturnFalse() throws Exception {
+        Lion lion = new Lion("Самка");
+        Assert.assertFalse(lion.doesHaveMane());
+    }
+
+
+
+    @Test
+    public void testFooThrowsIndexOutOfBoundsException() {
+        try {
+            Lion lion = new Lion("Test");
+            lion.doesHaveMane();
+            fail("expected exception was not occured.");
+        } catch(Exception e) {
+            System.out.println("exception is here");
+        }
+    }
+
+
+}
